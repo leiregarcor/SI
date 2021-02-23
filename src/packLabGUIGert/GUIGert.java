7422f9da105306ea8a26a08883da4807e5f09977
leire <leiregarciaupv@gmail.com>
2021-02-23 20:25:50 +0100

    fa

--- src/packLabGUIGert/GUIGert.java
+++ src/packLabGUIGert/GUIGert.java
@@ -4,16 +4,11 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class GUIGert extends JPanel {
-	private JTextField textField;
 
 	/**
 	 * Create the panel.
 	 */
 	public GUIGert() {
-		
-		textField = new JTextField();
-		add(textField);
-		textField.setColumns(10);
 
 	}
 
