cb340e1b0ddc636e4ef9142783f21604a34517ba
leire <leiregarciaupv@gmail.com>
2021-02-24 14:04:01 +0100

    creo que bien

--- src/packLabGUIGert/GUIGert.java
+++ src/packLabGUIGert/GUIGert.java
@@ -1,15 +1,67 @@
 package packLabGUIGert;
 
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
 
-public class GUIGert extends JPanel {
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+
+public class GUIGert extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
 
 	/**
-	 * Create the panel.
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			GUIGert dialog = new GUIGert();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
 	 */
 	public GUIGert() {
-
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);{
+			
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+			{
+				JLabel label = new JLabel("                                      ");
+				buttonPane.add(label);
+			}
+		}
+		setLocationRelativeTo(null);
 	}
 
 }
